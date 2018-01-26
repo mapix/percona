188d2e9ecd7dffbc4ef2c697808f06457fb87773
luoweifeng <luoweifeng@zhihu.com>
2018-01-26 17:31:53 +0800

    support schema init

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -162,6 +162,15 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		fi
 
 		echo
+		for f in /docker-entrypoint-initschema.d/*; do
+			case "$f" in
+				*.sh)     echo "$0: running $f"; . "$f" ;;
+				*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
+				*)        echo "$0: ignoring $f" ;;
+			esac
+			echo
+		done
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
 				*.sh)     echo "$0: running $f"; . "$f" ;;
